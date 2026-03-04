c518c5acc8d05bddcb31a3146bc9735ebeb50ef2
Karoliine Jakovleva <karoliinejakovleva@gmail.com>
2026-03-04 22:25:25 +0200

    Add pseudocode comments

--- greeting.py
+++ greeting.py
@@ -1,8 +1,14 @@
-last_name = input("Kirjuta oma perekonnanimi: ")
-letter = input("Vali sugu M/N: ")
-if letter == "M":
-    print(f"Tere, härra {last_name}!")
-elif letter == "N":
-    print(f"Tere, Proua {last_name}!")
-else:
-    print(f"Tere tulemast, {last_name} (sugu ei olegi tähtis).") 
+# Programm Greeting
+
+# Algus
+
+last_name = input("Kirjuta oma perekonnanimi: ") #Küsi kasutajalt perekonnanimi
+letter = input("Vali sugu M/N: ") #Küsi kasutajalt sugu
+if letter == "M": #Kui sugu on mees, siis
+    print(f"Tere, härra {last_name}!") #väljasta "Tere, härra [perekonnanimi]!"
+elif letter == "N": #Kui sugu on naine, siis
+    print(f"Tere, Proua {last_name}!") #väljasta "Tere, proua [perekonnanimi]!"
+else: #Muidu
+    print(f"Tere tulemast, {last_name} (sugu ei olegi tähtis).")  #väljasta "Tere tulemast, [perekonnanimi] (sugu ei olegi tähtis)!"
+
+# Lõpp
